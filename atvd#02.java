0dfb094661bc4f4a62a66f70d77fe7c07068e0ba
vigvx <pedrovigards0409@gmail.com>
2025-04-04 19:34:16 -0300

    atvd#02.java

---
+++ atvd#02.java
@@ -0,0 +1,9 @@
+public class NumeroPositivo {
+    public static void main(String[] args) {
+        int numero = 7;
+
+        if (numero > 0) {
+            System.out.println(numero + " é um número positivo.");
+        }
+    }
+}
