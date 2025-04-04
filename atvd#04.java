4360bba0ea2a111b081f28033ebab3200c975321
vigvx <pedrovigards0409@gmail.com>
2025-04-04 19:38:52 -0300

    Create atvd#04.java

---
+++ atvd#04.java
@@ -0,0 +1,11 @@
+public class ParOuImpar {
+    public static void main(String[] args) {
+        int numero = 5;
+
+        if (numero % 2 == 0) {
+            System.out.println(numero + " é um número par.");
+        } else {
+            System.out.println(numero + " é um número ímpar.");
+        }
+    }
+}
