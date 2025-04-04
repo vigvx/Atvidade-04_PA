2d6d478b6eeb8675ff5d963f6f83cb1e30f111c3
vigvx <pedrovigards0409@gmail.com>
2025-04-04 20:05:16 -0300

    Create atvd#09.java

---
+++ atvd#09.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String nome = "João";
+        int idade = 25;
+        
+        System.out.println(nome + " " + idade);
+    }
+}
