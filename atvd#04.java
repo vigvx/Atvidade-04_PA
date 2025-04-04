1c6cee72e44defb0b5caa3b149e809635529b2a6
vigvx <pedrovigards0409@gmail.com>
2025-04-04 19:52:59 -0300

    atvd#04.java

--- atvd#04.java
+++ atvd#04.java
@@ -1,11 +1,11 @@
 public class ParOuImpar {
     public static void main(String[] args) {
-        int numero = 5;
+        int num = 5;
 
         if (numero % 2 == 0) {
-            System.out.println(numero + " é um número par.");
+            System.out.println(num + " é um número par.");
         } else {
-            System.out.println(numero + " é um número ímpar.");
+            System.out.println(num + " é um número ímpar.");
         }
     }
 }
