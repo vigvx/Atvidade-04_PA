7cf6903c08dfbd8d956ad412a0660193b75cf319
vigvx <pedrovigards0409@gmail.com>
2025-04-04 20:19:42 -0300

    atvd#10.java

---
+++ atvd#10.java
@@ -0,0 +1,12 @@
+public class VerinNumInt {
+    public static void main(String[] args) {
+        double num1 = 55;
+        double num2 = 45;
+      double soma = num1 + num2;
+   if (soma > 50) {
+            System.out.println("A soma é maior que 50: " + soma);
+        } else {
+            System.out.println("A soma é 50 ou menor: " + soma);
+        }
+    }
+}
