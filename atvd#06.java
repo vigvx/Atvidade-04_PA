d72718d8ed88efce61aa9281ad8b268920f26a31
vigvx <pedrovigards0409@gmail.com>
2025-04-04 19:41:49 -0300

    atvd#06.java

---
+++ atvd#06.java
@@ -0,0 +1,10 @@
+public class SomaNumeros {
+    public static void main(String[] args) {
+        int numero1 = 10;
+        int numero2 = 20;
+        
+        int soma = numero1 + numero2;
+
+        System.out.println("A soma de " + numero1 + " e " + numero2 + " é: " + soma);
+    }
+}
