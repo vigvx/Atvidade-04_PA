d6a3a809c0ec7488b48a5c72b9d212a401a564bb
vigvx <pedrovigards0409@gmail.com>
2025-04-04 19:53:21 -0300

    atvd#03.java

--- atvd#03.java
+++ atvd#03.java
@@ -1,9 +1,9 @@
 public class MaiorQue100 {
     public static void main(String[] args) {
-        int numero = 150;
+        int num = 150;
 
-        if (numero > 100) {
-            System.out.println(numero + " é maior que 100.");
+        if (num > 100) {
+            System.out.println(num + " é maior que 100.");
         }
     }
 }
