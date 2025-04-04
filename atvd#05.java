87c06dc9a227dc51d545531cf3342d60844db43a
vigvx <pedrovigards0409@gmail.com>
2025-04-04 19:43:12 -0300

    atvd#05.java

--- atvd#05.java
+++ atvd#05.java
@@ -1,6 +1,6 @@
 public class IdadeEntre18e65 {
     public static void main(String[] args) {
-        int idade = 25; // Você pode alterar este valor para testar outras idades
+        int idade = 25;
 
         if (idade >= 18 && idade <= 65) {
             System.out.println("A pessoa tem idade entre 18 e 65 anos.");
