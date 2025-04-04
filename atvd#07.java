e82b45d5a883dbe2bb2efd56a9d943e40706442e
vigvx <pedrovigards0409@gmail.com>
2025-04-04 19:48:26 -0300

    atvd#07.java

---
+++ atvd#07.java
@@ -0,0 +1,10 @@
+public class Multiplicacao {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 4;
+        
+        int multiplicacao = num1 * num2;
+
+        System.out.println("O resultado da multiplicação de " + numero1 + " e " + numero2 + " é: " + multiplicacao);
+    }
+}
