06af31615a07471c9ccee7391061ab24cd424f5d
vigvx <pedrovigards0409@gmail.com>
2025-04-04 19:50:27 -0300

    atvd#08.java

---
+++ atvd#08.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int numeroInteiro = 5;
+        double numeroDecimal = 2.5;
+        
+        double soma = numeroInteiro + numeroDecimal;
+        
+        System.out.println("A soma é: " + soma);
+    }
+}
